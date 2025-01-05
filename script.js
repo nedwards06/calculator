de576a466bac62a91ea2c1222f9abadff3a16c34
Nathan Edwards <189742180+nedwards06@users.noreply.github.com>
2025-01-05 07:30:04 -0500

    Stub out basic functions and elements
    Create the basic functions for operating on
    given values, as well as HTML elements for
    the calculator display and buttons.

---
+++ script.js
@@ -0,0 +1,32 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function operate (n1, n2, op) {
+    switch(op) {
+        case '+':
+            return add (n1, n2);
+        case '-':
+            return subtract (n1, n2);
+        case '*':
+            return multiply (n1, n2);
+        case '/':
+            return divide (n1, n2);
+    }
+}
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
