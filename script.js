99db98ef4d709cd616f18917e9e0c404d173ed79
Nathan Edwards <189742180+nedwards06@users.noreply.github.com>
2025-01-08 08:01:03 -0500

    Create logic to control the display
    Display the value of a button when it is clicked,
    or reset the value to 0 if "Clear" is clicked.
    Store the current display value in a variable
    for later use.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+let currentDisplay = '0';
+
+const buttons = document.querySelectorAll('button');
+for (i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click', (event) => {
+        populateDisplay(event.target.id);
+    })
+};
+
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+
+function populateDisplay (value) {
+    if (currentDisplay === '0') {
+        //replace placeholder (0) if that's the only value
+        document.getElementById("display").textContent = value;
+    } else if (value === 'clear') {
+        //reset display to 0 when "clear" button is clicked
+        document.getElementById("display").textContent = '0';
+    } else {
+        //else add to current value
+        document.getElementById("display").textContent += value;
+    }
+    //update currentDisplay variable
+    currentDisplay = document.getElementById("display").textContent;
+}
+
 
 function operate (n1, n2, op) {
     switch(op) {
